c8ab220efe3f23753f4658064e8a2451f35ebac8
Christian Agüeros <chrisam1996@gmail.com>
2024-07-30 11:27:54 -0600

    Commit #36
    - Cambios en el orden de scaneo de @ComponentScan

--- Prueba_Tecnica_-_AdeA_Mexico/src/main/java/csam/pruebatecnica/adea/inicio/PrbTecAdeAApplication.java
+++ Prueba_Tecnica_-_AdeA_Mexico/src/main/java/csam/pruebatecnica/adea/inicio/PrbTecAdeAApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan(basePackages = {"csam.pruebatecnica.adea.controller", "csam.pruebatecnica.adea.service", "csam.pruebatecnica.adea.dao", 
-		"csam.pruebatecnica.adea.configuration","csam.pruebatecnica.adea.model"} )
+@ComponentScan(basePackages = {
+		"csam.pruebatecnica.adea.configuration",
+		"csam.pruebatecnica.adea.controller", 
+		"csam.pruebatecnica.adea.service", 
+		"csam.pruebatecnica.adea.dao", 
+		"csam.pruebatecnica.adea.model"} )
 @EntityScan (basePackages = {"csam.pruebatecnica.adea.model"})
 @EnableJpaRepositories (basePackages = {"csam.pruebatecnica.adea.dao"} )
 @SpringBootApplication
