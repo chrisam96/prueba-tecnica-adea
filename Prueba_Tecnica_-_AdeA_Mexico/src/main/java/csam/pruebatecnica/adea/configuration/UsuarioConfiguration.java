db3f94bbccd8e1788880c1ff2ac2c87f587dc6a1
Christian Agüeros <chrisam1996@gmail.com>
2024-07-31 03:20:55 -0600

    Commit #44
    + El Bean de CorsConfigSource se pasa a la clae principal

--- Prueba_Tecnica_-_AdeA_Mexico/src/main/java/csam/pruebatecnica/adea/configuration/UsuarioConfiguration.java
+++ Prueba_Tecnica_-_AdeA_Mexico/src/main/java/csam/pruebatecnica/adea/configuration/UsuarioConfiguration.java
@@ -22,34 +22,5 @@ public class UsuarioConfiguration {
 	
 	//Configuración de las politicas de seguridad CORS de la aplicación
 	
-	@Bean
-	public CorsConfigurationSource  corsConfigurationSource() {
 		
-		CorsConfiguration config = new CorsConfiguration();
-		
-		config.setAllowedOriginPatterns( Arrays.asList("*") );
-		//config.addAllowedOrigin("*");
-		config.setAllowedMethods( Arrays.asList(
-				RequestMethod.GET.name(),
-				RequestMethod.POST.name(),
-				RequestMethod.PUT.name(),
-				RequestMethod.DELETE.name(),
-				RequestMethod.HEAD.name()
-				) );
-		config.setAllowedHeaders( Arrays.asList("*") );
-		config.setExposedHeaders( Arrays.asList("*") );
-		
-		//config.setMaxAge(Duration.ZERO);
-		//config.setAllowCredentials(Boolean.TRUE);
-		
-		// Instacia que extiende de CorsConfigurationSpurce.
-		// Esta clase que permite registrar configuraciones CORS basadas en patrones de URL.
-		UrlBasedCorsConfigurationSource corsSource = new UrlBasedCorsConfigurationSource();
-		
-		corsSource.registerCorsConfiguration("/**", config);
-		
-		//return new CorsFilter(corsSource);
-		return corsSource;
-	}  
-	
 }
--- Prueba_Tecnica_-_AdeA_Mexico/src/main/java/csam/pruebatecnica/adea/inicio/PrbTecAdeAApplication.java
+++ Prueba_Tecnica_-_AdeA_Mexico/src/main/java/csam/pruebatecnica/adea/inicio/PrbTecAdeAApplication.java
@@ -1,10 +1,19 @@
 package csam.pruebatecnica.adea.inicio;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @ComponentScan(basePackages = {
 		"csam.pruebatecnica.adea.configuration",
@@ -20,5 +29,39 @@ public class PrbTecAdeAApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PrbTecAdeAApplication.class, args);
 	}
+	
+	
+	@Bean
+	@Order(Ordered.HIGHEST_PRECEDENCE)
+	public CorsConfigurationSource  corsConfigurationSource() {
+		
+		CorsConfiguration config = new CorsConfiguration();
+		
+		config.setAllowedOriginPatterns( Arrays.asList("*") );
+		//config.addAllowedOrigin("*");
+		config.setAllowedMethods( Arrays.asList(
+				/*RequestMethod.GET.name(),
+				RequestMethod.POST.name(),
+				RequestMethod.PUT.name(),
+				RequestMethod.DELETE.name(),
+				RequestMethod.HEAD.name()*/
+				"*"
+				) );
+		config.setAllowedHeaders( Arrays.asList("*") );
+		config.setExposedHeaders( Arrays.asList("*") );
+		
+		//config.setMaxAge(Duration.ZERO);
+		//config.setAllowCredentials(Boolean.TRUE);
+		
+		// Instacia que extiende de CorsConfigurationSpurce.
+		// Esta clase que permite registrar configuraciones CORS basadas en patrones de URL.
+		UrlBasedCorsConfigurationSource corsSource = new UrlBasedCorsConfigurationSource();
+		
+		corsSource.registerCorsConfiguration("/**", config);
+		
+		//return new CorsFilter(corsSource);
+		return corsSource;
+	}  
+
 
 }
